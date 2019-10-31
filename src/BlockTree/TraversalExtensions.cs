e97b0d9d53a2f4ce5905c83457a2f3fad98a995a
NickStrupat <nick.strupat@gmail.com>
2019-10-31 13:42:49 -0400

    work on removing the tree data structure from block tree. it just complicates things

---
+++ src/BlockTree/TraversalExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockTree
+{
+	class TraversalExtensions
+	{
+		public static IEnumerable<T> TraverseDepthFirst<T>(T item, Func<T, IEnumerable<T>> childSelector)
+		{
+			var stack = new Stack<T>();
+			stack.Push(item);
+			while (stack.Count != 0)
+			{
+				var next = stack.Pop();
+				yield return next;
+				foreach (var child in childSelector(next))
+					stack.Push(child);
+			}
+		}
+
+		public static IEnumerable<T> TraverseBreadthFirst<T>(T item, Func<T, IEnumerable<T>> childSelector)
+		{
+			var stack = new Queue<T>();
+			stack.Enqueue(item);
+			while (stack.Count != 0)
+			{
+				var next = stack.Dequeue();
+				yield return next;
+				foreach (var child in childSelector(next))
+					stack.Enqueue(child);
+			}
+		}
+	}
+}
